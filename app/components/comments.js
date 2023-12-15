508d6931d385e04670b84e4de336f08391aae8b4
arthuranteater <25356102+arthuranteater@users.noreply.github.com>
2023-12-15 09:43:14 -0600

    upd env vars local, vercel

--- app/components/comments.js
+++ app/components/comments.js
@@ -2,8 +2,8 @@ import Comment from "./comment";
 import { getClient } from "./apollo-client";
 
 export default async function CommentContainer({ comments }) {
-  const { data } = await getClient().query({ query: userQuery });
-  console.log(data);
+  //   const { data } = await getClient().query({ query: userQuery });
+  //   console.log(data);
   return (
     <ul className="w-full mt-6 flex flex-wrap gap-10 justify-center">
       {comments.map((comment) => (
